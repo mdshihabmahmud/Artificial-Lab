4c0e1934907ba132886af00572f695023a083a61
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-03-02 00:19:35 +0600

    Create Load the datasets.py

---
+++ Load the datasets.py
@@ -0,0 +1,3 @@
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
